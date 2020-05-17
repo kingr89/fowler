4ee23b24c71d9130073e7d0af3ecb332a3f72d6e
rking <robinking89m@gmail.com>
2020-05-17 12:48:56 +0200

    renamed method moved method getCharge to Rental

--- src/Customer.java
+++ src/Customer.java
@@ -18,47 +18,27 @@ class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
+        double totalCharge = 0;
         int frequentRenterPoints = 0;
         StringBuilder result = new StringBuilder("Rental Record for " + this.getName() + "\n");
         result.append("\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n");
 
         for (Rental rental:rentals) {
             //determine amounts for each line
-            double thisAmount = amountFor(rental);
+            double rentalCharge = rental.getCharge();
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
             if ((rental.getMovie().getPriceCode() == PriceCode.NEW_RELEASE) && rental.getDaysRented() > 1)
                 frequentRenterPoints++;
             //show figures for this rental
-            result.append("\t").append(rental.getMovie().getTitle()).append("\t").append("\t").append(rental.getDaysRented()).append("\t").append(thisAmount).append("\n");
-            totalAmount += thisAmount;
+            result.append("\t").append(rental.getMovie().getTitle()).append("\t").append("\t").append(rental.getDaysRented()).append("\t").append(rentalCharge).append("\n");
+            totalCharge += rentalCharge;
         }
         //add footer lines
-        result.append("Amount owed is ").append(totalAmount).append("\n");
+        result.append("Amount owed is ").append(totalCharge).append("\n");
         result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
         return result.toString();
-    }
-
-    private double amountFor(Rental rental) {
-        double thisAmount = 0;
-        switch (rental.getMovie().getPriceCode()) {
-            case REGULAR:
-                thisAmount += 2;
-                if (rental.getDaysRented() > 2)
-                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                thisAmount += rental.getDaysRented() * 3;
-                break;
-            case CHILDRENS:
-                thisAmount += 1.5;
-                if (rental.getDaysRented() > 3)
-                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
     }
 
 }
--- src/Rental.java
+++ src/Rental.java
@@ -1,14 +1,36 @@
 class Rental {
-    private Movie movie;
-    private int daysRented;
-    public Rental(Movie newmovie, int newdaysRented) {
-        movie = newmovie;
-        daysRented = newdaysRented;
+    private final Movie movie;
+    private final int daysRented;
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
     }
     public int getDaysRented() {
         return daysRented;
     }
     public Movie getMovie() {
         return movie;
+    }
+
+    public double getCharge() {
+        double charge = 0;
+        switch (movie.getPriceCode()) {
+            case REGULAR:
+                charge += 2;
+                if (daysRented > 2)
+                    charge += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                charge += daysRented * 3;
+                break;
+            case CHILDRENS:
+                charge += 1.5;
+                if (daysRented > 3)
+                    charge += (daysRented - 3) * 1.5;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + movie.getPriceCode());
+        }
+        return charge;
     }
 }
